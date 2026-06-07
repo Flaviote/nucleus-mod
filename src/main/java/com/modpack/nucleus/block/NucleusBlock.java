0bafbae682a2b90e608bf091254e6ee8d5310080
Flaviote <flaviomesasantana@gmail.com>
2026-06-07 18:52:51 +0100

    Update NucleusBlock.java

--- src/main/java/com/modpack/nucleus/block/NucleusBlock.java
+++ src/main/java/com/modpack/nucleus/block/NucleusBlock.java
@@ -2,12 +2,17 @@ package com.modpack.nucleus.block;
 
 import com.modpack.nucleus.entity.NucleusBlockEntity;
 import com.modpack.nucleus.init.NucleusBlockEntities;
+import com.modpack.nucleus.state.NucleusStateManager;
 import com.mojang.serialization.MapCodec;
 import eu.pb4.polymer.core.api.block.PolymerBlock;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -46,10 +51,16 @@ public class NucleusBlock extends BlockWithEntity implements PolymerBlock {
     }
 
     @Override
-    public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!world.isClient && world.getBlockEntity(pos) instanceof NucleusBlockEntity be) {
-            be.clearOwner();
-        }
-        return super.onBreak(world, pos, state, player);
-    }
-}
+    public void onPlaced(World world, BlockPos pos, BlockState state,
+                         @Nullable LivingEntity placer, ItemStack itemStack) {
+        super.onPlaced(world, pos, state, placer, itemStack);
+
+        if (world.isClient) return;
+        if (!(placer instanceof ServerPlayerEntity player)) return;
+
+        double dist = Math.sqrt(pos.getX() * pos.getX() + pos.getZ() * pos.getZ());
+        if (dist <= 50) {
+            world.breakBlock(pos, false);
+            player.getInventory().insertStack(new ItemStack(this));
+            player.sendMessage(Text.literal(
+                "§c✗ No puedes colocar tu Núcleo dentro del lobby."), false);
