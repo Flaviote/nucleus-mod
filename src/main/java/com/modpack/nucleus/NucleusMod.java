c38354ef0a1cbb8ebe238239735cee57917d6560
Flaviote <flaviomesasantana@gmail.com>
2026-06-07 14:10:52 +0100

    Update NucleusMod.java

--- src/main/java/com/modpack/nucleus/NucleusMod.java
+++ src/main/java/com/modpack/nucleus/NucleusMod.java
@@ -1,48 +1,27 @@
-package com.modpack.nucleus.block;
+package com.modpack.nucleus;
 
-import com.modpack.nucleus.entity.NucleusBlockEntity;
+import com.modpack.nucleus.init.NucleusBlocks;
 import com.modpack.nucleus.init.NucleusBlockEntities;
-import eu.pb4.polymer.core.api.block.PolymerBlock;
-import net.minecraft.block.*;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class NucleusBlock extends BlockWithEntity implements PolymerBlock {
+public class NucleusMod implements ModInitializer {
 
-    public NucleusBlock(Settings settings) {
-        super(settings);
-    }
+    public static final String MOD_ID = "nucleus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public Block getPolymerBlock(BlockState state) {
-        return Blocks.BEACON;
+    public void onInitialize() {
+        NucleusBlocks.register();
+        NucleusBlockEntities.register();
+        PolymerResourcePackUtils.markAsRequired();
+        LOGGER.info("[Nucleus] Mod inicializado correctamente.");
     }
 
-    @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
-    }
-
-    @Nullable
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new NucleusBlockEntity(pos, state);
-    }
-
-    @Override
-    public BlockEntityType<NucleusBlockEntity> getPolymerBlockEntity() {
-        return NucleusBlockEntities.NUCLEUS_BLOCK_ENTITY;
-    }
-
-    @Override
-    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!world.isClient && world.getBlockEntity(pos) instanceof NucleusBlockEntity be) {
-            be.clearOwner();
-        }
-        super.onBreak(world, pos, state, player);
+    public static Identifier id(String path) {
+        return Identifier.of(MOD_ID, path);
     }
 }
